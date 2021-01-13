2243d258631c9595c331808cc705f648e2389a99
Will Barley <will.barley@gmail.com>
2021-01-13 11:39:53 -0600

    feat: add Vuetify for buttons and things

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <h1>Welcome!</h1>
-    <h2>
-      We {{ haveAuthInformation ? "DO" : "do NOT" }} have auth information for
-      you.
-    </h2>
-    <ul v-if="haveAuthInformation">
-      <li>id_token: {{ id_token }}</li>
-      <li>access_token: {{ access_token }}</li>
-      <li>token_type: {{ token_type }}</li>
-      <li>expires_in: {{ expires_in }}</li>
-    </ul>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
 
-    <HelloWorld />
-  </div>
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from "./components/HelloWorld";
 
 export default {
   name: "App",
+
   components: {
     HelloWorld
   },
-  data: function() {
-    return {
-      id_token: null,
-      access_token: null,
-      expires_in: null,
-      token_type: null
-    };
-  },
-  computed: {
-    haveAuthInformation: function() {
-      return (
-        // must have all
-        !!this.id_token &&
-        !!this.access_token &&
-        !!this.expires_in &&
-        !!this.token_type
-      );
-    }
-  },
-  created: function() {
-    // We need to get authentication information from the URL Fragments provided
-    //  AWS Cognito is going to ba passing Authentication tokens
-    //  via URL Fragments AKA Hash (not URL Query Parameters)
-    const fragments = window.location.hash.substring(1); // remove leading "#"
-    const params = new URLSearchParams(fragments); // The Fragments are in the same format as Query/Search Params
-    console.debug(`Fragments provided:`);
-    for (const [key, value] of params.entries()) {
-      console.debug(`${key}=${value}`);
-    }
-    this.id_token = params.get("id_token");
-    this.access_token = params.get("access_token");
-    this.expires_in = params.get("expires_in");
-    this.token_type = params.get("token_type");
-  }
+
+  data: () => ({
+    //
+  })
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,30 +1,80 @@
 <template>
-  <div class="hello"></div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome!
+        </h1>
+        <h2>
+          We {{ haveAuthInformation ? "DO" : "do NOT" }} have auth information
+          for you.
+        </h2>
+        <ul v-if="haveAuthInformation">
+          <li>id_token: {{ id_token }}</li>
+          <li>access_token: {{ access_token }}</li>
+          <li>token_type: {{ token_type }}</li>
+          <li>expires_in: {{ expires_in }}</li>
+        </ul>
+        <v-btn
+          v-else
+          block
+          color="primary"
+          elevation="8"
+          large
+          href="https://socotra.auth.us-east-1.amazoncognito.com/login?response_type=token&client_id=3pcr6kgujtv3e4esatvlkc5ukk&redirect_uri=http://localhost:8081"
+          >Authenticate</v-btn
+        >
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
+
   data: function() {
-    return {};
+    return {
+      id_token: null,
+      access_token: null,
+      expires_in: null,
+      token_type: null
+    };
+  },
+  computed: {
+    haveAuthInformation: function() {
+      return (
+        // must have all
+        !!this.id_token &&
+        !!this.access_token &&
+        !!this.expires_in &&
+        !!this.token_type
+      );
+    }
+  },
+  created: function() {
+    // We need to get authentication information from the URL Fragments provided
+    //  AWS Cognito is going to ba passing Authentication tokens
+    //  via URL Fragments AKA Hash (not URL Query Parameters)
+    const fragments = window.location.hash.substring(1); // remove leading "#"
+    const params = new URLSearchParams(fragments); // The Fragments are in the same format as Query/Search Params
+    console.debug(`Fragments provided:`);
+    for (const [key, value] of params.entries()) {
+      console.debug(`${key}=${value}`);
+    }
+    this.id_token = params.get("id_token");
+    this.access_token = params.get("access_token");
+    this.expires_in = params.get("expires_in");
+    this.token_type = params.get("token_type");
   }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
